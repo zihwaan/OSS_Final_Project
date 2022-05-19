3bc9b951e257d9eaa2ea533dc59c9300c8cdf5ca
변지환 <optimize21@g.skku.edu>
2022-05-20 00:48:40 +0900

    기초 프론트 작업

--- oss/src/App.js
+++ oss/src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Header = styled.div`
+  background-color: black;
+  align-items: flex-end;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Big = styled.text`
+  font-size: 30px;
+  color: white;
+  align-self: center;
+`;
+
+const Foo = styled.div`
+  background-color: black;
+  align-items: flex-end;
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  margin-top: auto;
+`;
+
+const Main = styled.div`
+  background-color: white;
+  justify-content: space-between;
+  height: 80vh;
+`;
+
+const Container1 = styled.div`
+  background-color: pink;
+  padding-top: 30px;
+  padding-left: 160px;
+  padding-right: 160px;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const Welcome = styled.text`
+  font-size: 25px;
+  align-self: center;
+  justify-self: center;
+`;
+
+const Container2 = styled.div`
+  background-color: grey;
+  align-items: center;
+  justify-content: center;
+  height: 30%;
+  text-align: center;
+`;
+
+const BTNPortfolio = styled.button`
+  background-color: blue;
+`;
+
+function Fun(){
+  console.log("Click!");
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header> 
+        <Big style={{marginLeft: "60px"}}>잡아라 잡!</Big> 
+        <Big style={{marginRight: "60px"}}>메뉴</Big> 
+      </Header>
+
+      <Main>
+        <Container1>
+          <Container2>
+            <Welcome> (Welcome Message and help) </Welcome>
+            <BTNPortfolio onClick={Fun}> <Big>포트폴리오 버튼</Big> </BTNPortfolio>
+          </Container2>
+        </Container1>
+      </Main>
+
+
+      <Foo> <Big>(Information)</Big> </Foo>
     </div>
   );
 }
