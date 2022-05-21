a208b88dca673ebadf74e771f234ff7034692126
0Cracker <gogoseo11084@gmail.com>
2022-05-22 01:41:32 +0900

    Update scripts.js

--- oss/pages/js/scripts.js
+++ oss/pages/js/scripts.js
@@ -31,14 +31,30 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
     // Profile
+    function saveResume(node){
+        task={
+            class:node.class,
+            value:node.value
+        }
+        localStorage.setItem("task",JSON.stringify(tasks));
+
+
+    }
     let finish_button=document.querySelector("#finishbt");
     finish_button.addEventListener("click",()=>{
-        let profile_input=document.querySelectorAll("#FirstName, #LastName, #Address, #Introduce");
-        let experience_input=document.querySelectorAll("#");
-        let education_input=document.querySelectorAll("#");
-        let skills_input=document.querySelectorAll("");
-        let interests_input=document.querySelectorAll("");
-        let awards_input=document.querySelectorAll("");
+        let profile_input=document.querySelectorAll("#FirstName, #LastName, #Address, #Introduce").forEach(saveResume);
+        let experience_input=document.querySelectorAll(".experience, .timeline, .subhead .experienceexp").forEach(saveResume);
+        let education_input=document.querySelectorAll(".education, .edutimeline, .edusubhead, .explainedu").forEach(saveResume);
+        let skills_input=document.querySelectorAll(".skills, .workflow").forEach(saveResume);
+        let interests_input=document.querySelectorAll(".interests").forEach(saveResume);
+        let awards_input=document.querySelectorAll(".awards").forEach(saveResume);
+        
+        console.log(profile_input[0].value,profile_input[1].value,profile_input[2].value,profile_input[3].value);
+        console.log(experience_input[0].value,experience_input[1].value,experience_input[2].value,experience_input[3].value);
+        //let education_input=document.querySelectorAll("");
+        //let skills_input=document.querySelectorAll("");
+        //let interests_input=document.querySelectorAll("");
+        //let awards_input=document.querySelectorAll("");
     })
 
     
